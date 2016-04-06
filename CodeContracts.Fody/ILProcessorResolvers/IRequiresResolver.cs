f54025427022f3ae63163c764cd6960c667d400a
Николай <gultuy00@gmail.com>
2016-04-07 00:22:36 +0300

    Добавлены интерфейсы для получения ILProcessor для определений контрактов

---
+++ CodeContracts.Fody/ILProcessorResolvers/IRequiresResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+using Mono.Cecil.Cil;
+
+namespace CodeContracts.Fody.ILProcessorResolvers
+{
+    public interface IRequiresResolver
+    {
+        ILProcessor Resolve(RequiresDefinition requiresDefinition);
+    }
+}
