11d1e7a2732d69fe162e52e33a7fe55d7933aa43
Николай <gultuy00@gmail.com>
2016-04-06 22:16:57 +0300

    Добавлена точка входа для выполнения плагина Fody

---
+++ CodeContracts.Fody/ModuleWeaver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody
+{
+    public class ModuleWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+        
+        public Action<string> LogDebug { get; set; }
+
+        public Action<string> LogInfo { get; set; }
+
+        public Action<string> LogWarning { get; set; }
+
+        public void Execute()
+        {
+
+        }
+    }
+}
