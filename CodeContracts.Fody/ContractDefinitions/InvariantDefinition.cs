0fbfd6ad572a2cda98ee9aa43d32acef6f562313
Николай <gultuy00@gmail.com>
2016-04-09 00:19:19 +0300

    Определение инварианта

--- CodeContracts.Fody/ContractDefinitions/InvariantDefinition.cs
+++ CodeContracts.Fody/ContractDefinitions/InvariantDefinition.cs
@@ -4,11 +4,17 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Cecil;
 
 namespace CodeContracts.Fody.ContractDefinitions
 {
     public class InvariantDefinition : ContractDefinition
     {
-
+        public InvariantDefinition(CustomAttribute contractAttribute, TypeDefinition declaringType) 
+            : base(contractAttribute, declaringType)
+        {
+            Contract.Requires(contractAttribute != null);
+            Contract.Requires(declaringType != null);
+        }
     }
 }
