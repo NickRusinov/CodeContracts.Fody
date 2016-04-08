392d397a127ab6da927b48f64a2ff0fc4728ab15
Николай <gultuy00@gmail.com>
2016-04-09 01:01:03 +0300

    Добавлены тесты для сканирования методов

---
+++ CodeContracts.Fody.Tests/ContractScanners/MethodScannerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractScanners;
+using Mono.Cecil;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CodeContracts.Fody.Tests.ContractScanners
+{
+    public class MethodScannerTests
+    {
+        [Theory(DisplayName = "Проверка сканирования всех параметров метода"), AutoFixture]
+        public void ScanAllParametersTest(
+            [Frozen]ModuleDefinition moduleDefinition,
+            [Frozen]Mock<IParameterScanner> parameterScannerMock,
+            MethodScanner sut)
+        {
+            sut.Scan(moduleDefinition.GetType(Constants.SithNamespace, "DarthMaul").Methods.Single(md => md.Name == "KillJedi")).ToList();
+
+            parameterScannerMock.Verify(ps => ps.Scan(It.IsAny<ParameterDefinition>()), Times.Exactly(2));
+        }
+
+        [Theory(DisplayName = "Проверка сканирования возвращаемого значения метода"), AutoFixture]
+        public void ScanAllMethodReturnTest(
+            [Frozen]ModuleDefinition moduleDefinition,
+            [Frozen]Mock<IMethodReturnScanner> methodReturnScannerMock,
+            MethodScanner sut)
+        {
+            sut.Scan(moduleDefinition.GetType(Constants.SithNamespace, "DarthMaul").Methods.Single(md => md.Name == "KillJedi")).ToList();
+
+            methodReturnScannerMock.Verify(mrs => mrs.Scan(It.IsAny<MethodReturnType>()), Times.Once);
+        }
+    }
+}
