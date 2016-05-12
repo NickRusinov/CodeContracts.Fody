2930f12e02817e9f297948a15b0ec8ea59c8ee9f
Николай <gultuy00@gmail.com>
2016-05-12 21:54:45 +0300

    Добавлен фейк класс для определения атрибута контракта

---
+++ CodeContracts.Fody/ContractScanners/ContractCriteria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.ContractScanners
+{
+    public class ContractCriteria : IContractCriteria
+    {
+        public bool IsContract(CustomAttribute attribute)
+        {
+            // todo - алгоритм распознавания атрибута контракта
+            return true;
+        }
+    }
+}
