815f029d5ffdd0bdb50c57fdcd806d2fe98d5291
Николай <gultuy00@gmail.com>
2016-05-12 20:13:14 +0300

    Добавлен проект для интеграционных тестов

---
+++ CodeContracts.Fody.IntegrationTests/AssemblyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeContracts.Fody.IntegrationTests
+{
+    public class AssemblyTests
+    {
+        [Fact(DisplayName = "Проверка корректности загрузки тестовой сборки")]
+        public void AssemblyTest()
+        {
+            Assembly.LoadFrom("StarWars.dll");
+        }
+    }
+}
