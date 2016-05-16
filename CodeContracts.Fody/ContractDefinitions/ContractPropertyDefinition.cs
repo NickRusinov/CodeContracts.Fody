9bd2bd87e65e994c7a3a1ee45a047242db2f7ac6
Николай <gultuy00@gmail.com>
2016-05-16 20:39:33 +0300

    Добавлено определение свойства атрибута контракта

---
+++ CodeContracts.Fody/ContractDefinitions/ContractPropertyDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContracts.Fody.ContractDefinitions
+{
+    public class ContractPropertyDefinition
+    {
+        public ContractPropertyDefinition(string propertyName, object propertyValue)
+        {
+            Contract.Requires(propertyName != null);
+
+            PropertyName = propertyName;
+            PropertyValue = propertyValue;
+        }
+
+        public string PropertyName { get; }
+
+        public object PropertyValue { get; }
+    }
+}
