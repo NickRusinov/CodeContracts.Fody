7cf4f25410fe16308a4709372ac1e60f652edaa2
Николай <gultuy00@gmail.com>
2016-05-17 22:24:28 +0300

    Добавлена фейковая реализация алгоритма сканирования атрибута контракта для нахождения методов валидации

---
+++ CodeContracts.Fody/ContractInjectors/ContractValidateScanner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class ContractValidateScanner : IContractValidateScanner
+    {
+        private readonly IContractValidateCriteria contractValidateCriteria;
+
+        public ContractValidateScanner(IContractValidateCriteria contractValidateCriteria)
+        {
+            Contract.Requires(contractValidateCriteria != null);
+
+            this.contractValidateCriteria = contractValidateCriteria;
+        }
+
+        public IEnumerable<ContractValidateDefinition> Scan(ContractDefinition contractDefinition)
+        {
+            // todo - алгоритм сканирования атрибута контракта для нахождения методов валидации
+            return Enumerable.Empty<ContractValidateDefinition>();
+        }
+    }
+}
---
+++ CodeContracts.Fody/ContractInjectors/IContractValidateScanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public interface IContractValidateScanner
+    {
+        IEnumerable<ContractValidateDefinition> Scan(ContractDefinition contractDefinition);
+    }
+}
