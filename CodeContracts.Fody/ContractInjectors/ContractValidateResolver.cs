9ac8cae6353d7ba96ea4a761a80293983baa4cf3
Николай <gultuy00@gmail.com>
2016-05-17 22:39:45 +0300

    Добавлена фейковая реализация алгоритма получения методов валидации контракта для метода

---
+++ CodeContracts.Fody/ContractInjectors/ContractValidateResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class ContractValidateResolver : IContractValidateResolver
+    {
+        public ILookup<ContractValidateDefinition, IParameterBuilder> Resolve(ContractDefinition contractDefinition, MethodDefinition methodDefinition)
+        {
+            // todo -  алгоритм получения методов валидации контракта для метода
+            return null;
+        }
+    }
+}
