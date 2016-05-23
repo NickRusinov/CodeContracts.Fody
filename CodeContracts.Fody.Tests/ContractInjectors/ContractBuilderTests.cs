e2857121100d732c308493f943918ec5cd147e15
Николай <gultuy00@gmail.com>
2016-05-23 21:54:05 +0300

    Добавлен построитель метода контракта

---
+++ CodeContracts.Fody.Tests/ContractInjectors/ContractBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractInjectors;
+using Mono.Cecil;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CodeContracts.Fody.Tests.ContractInjectors
+{
+    public class ContractBuilderTests
+    {
+        [Theory(DisplayName = "Проверка вызова фабрики создания построителя метода валидации контракта для построителя контракта"), AutoFixture]
+        public void ContractMethodFactoryHasBeenCalledTest(
+            [Frozen] ModuleDefinition moduleDefinition,
+            [Frozen] Mock<IContractMethodFactory> contractMethodFactoryMock,
+            ContractValidate contractValidate,
+            ContractBuilder sut)
+        {
+            var instructions = sut.Build(contractValidate).ToList();
+
+            contractMethodFactoryMock.Verify(cmf => cmf.Create(moduleDefinition, contractValidate.ValidateDefinition.ExceptionType, contractValidate.ValidateDefinition.ErrorMessage), Times.Once);
+            Assert.NotEmpty(instructions);
+        }
+    }
+}
---
+++ CodeContracts.Fody/ContractInjectors/ContractBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil.Cil;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class ContractBuilder : IInstructionsBuilder
+    {
+        private readonly IContractMethodFactory contractMethodFactory;
+
+        public ContractBuilder(IContractMethodFactory contractMethodFactory)
+        {
+            Contract.Requires(contractMethodFactory != null);
+
+            this.contractMethodFactory = contractMethodFactory;
+        }
+
+        public IEnumerable<Instruction> Build(ContractValidate contractValidate)
+        {
+            return contractMethodFactory.Create(contractValidate.ValidateDefinition.ValidateMethod.Module, contractValidate.ValidateDefinition.ExceptionType, contractValidate.ValidateDefinition.ErrorMessage).Build(contractValidate);
+        }
+    }
+}
