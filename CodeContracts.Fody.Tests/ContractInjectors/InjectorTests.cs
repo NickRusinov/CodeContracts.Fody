65a1e03c27a94e3146f41211c27f8c0ef94cd957
Николай <gultuy00@gmail.com>
2016-05-24 19:35:39 +0300

    Добавлены инжекторы для внедрения предусловий, постусловий и инвариантов

---
+++ CodeContracts.Fody.Tests/ContractInjectors/InjectorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+using CodeContracts.Fody.ContractInjectors;
+using Mono.Cecil;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CodeContracts.Fody.Tests.ContractInjectors
+{
+    public class InjectorTests
+    {
+        [Theory(DisplayName = "Проверка вызова получателя методов валидации контрактов при внедрении контрактов"), AutoFixture]
+        public void ContractValidatesResolverHasBeenCalledTest(
+            [Frozen] Mock<IContractValidatesResolver> contractValidatesResolverMock,
+            ContractDefinition contractDefinition,
+            MethodDefinition methodDefinition,
+            Injector sut)
+        {
+            sut.Inject(contractDefinition, methodDefinition);
+
+            contractValidatesResolverMock.Verify(cvr => cvr.Resolve(contractDefinition, methodDefinition), Times.Once);
+        }
+
+        [Theory(DisplayName = "Проверка вызова получателя команд для контрактов при внедрении контрактов"), AutoFixture]
+        public void InstructionsBuilderHasBeenCalledTest(
+            [Frozen] Mock<IInstructionsBuilder> instructionsBuilderMock,
+            ContractDefinition contractDefinition,
+            MethodDefinition methodDefinition,
+            Injector sut)
+        {
+            sut.Inject(contractDefinition, methodDefinition);
+
+            instructionsBuilderMock.Verify(ib => ib.Build(It.IsAny<ContractValidate>()), Times.Exactly(3));
+        }
+    }
+}
---
+++ CodeContracts.Fody/ContractInjectors/Injector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeContracts.Fody.ContractDefinitions;
+using CodeContracts.Fody.Internal;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class Injector : IInjector
+    {
+        private readonly IContractValidatesResolver contractValidatesResolver;
+
+        private readonly IInstructionsBuilder instructionsBuilder;
+
+        public Injector(IContractValidatesResolver contractValidatesResolver, IInstructionsBuilder instructionsBuilder)
+        {
+            Contract.Requires(contractValidatesResolver != null);
+            Contract.Requires(instructionsBuilder != null);
+
+            this.contractValidatesResolver = contractValidatesResolver;
+            this.instructionsBuilder = instructionsBuilder;
+        }
+
+        public void Inject(ContractDefinition contractDefinition, MethodDefinition methodDefinition)
+        {
+            var instructions = contractValidatesResolver.Resolve(contractDefinition, methodDefinition).SelectMany(instructionsBuilder.Build);
+
+            methodDefinition.Body.Instructions.AddRange(instructions);
+        }
+    }
+}
