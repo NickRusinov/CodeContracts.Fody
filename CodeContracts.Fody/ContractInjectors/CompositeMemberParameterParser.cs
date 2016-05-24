926cdfd1380237f49c535a1315695e54175580f7
Николай <gultuy00@gmail.com>
2016-05-24 20:42:07 +0300

    Добавлены компоновщики для парсеров

---
+++ CodeContracts.Fody/ContractInjectors/CompositeMemberParameterParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class CompositeMemberParameterParser : IMemberParameterParser
+    {
+        private readonly IEnumerable<IMemberParameterParser> memberParameterParsers;
+
+        public CompositeMemberParameterParser(IEnumerable<IMemberParameterParser> memberParameterParsers)
+        {
+            Contract.Requires(memberParameterParsers != null);
+
+            this.memberParameterParsers = memberParameterParsers;
+        }
+
+        public CompositeMemberParameterParser(params IMemberParameterParser[] memberParameterParsers)
+            : this(memberParameterParsers as IEnumerable<IMemberParameterParser>)
+        {
+            Contract.Requires(memberParameterParsers != null);
+        }
+
+        public ParseResult Parse(TypeDefinition typeDefinition, string parameterString)
+        {
+            return memberParameterParsers.Select(mpp => mpp.Parse(typeDefinition, parameterString))
+                .FirstOrDefault(pr => !Equals(pr, ParseResult.Empty));
+        }
+    }
+}
---
+++ CodeContracts.Fody/ContractInjectors/CompositeMethodParameterParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.ContractInjectors
+{
+    public class CompositeMethodParameterParser : IMethodParameterParser
+    {
+        private readonly IEnumerable<IMethodParameterParser> methodParameterParsers;
+
+        public CompositeMethodParameterParser(IEnumerable<IMethodParameterParser> methodParameterParsers)
+        {
+            Contract.Requires(methodParameterParsers != null);
+
+            this.methodParameterParsers = methodParameterParsers;
+        }
+
+        public CompositeMethodParameterParser(params IMethodParameterParser[] methodParameterParsers)
+            : this(methodParameterParsers as IEnumerable<IMethodParameterParser>)
+        {
+            Contract.Requires(methodParameterParsers != null);
+        }
+
+        public ParseResult Parse(MethodDefinition methodDefinition, string parameterString)
+        {
+            return methodParameterParsers.Select(mpp => mpp.Parse(methodDefinition, parameterString))
+                .FirstOrDefault(pr => !Equals(pr, ParseResult.Empty));
+        }
+    }
+}
