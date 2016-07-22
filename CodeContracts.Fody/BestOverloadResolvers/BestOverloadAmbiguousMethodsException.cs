85de88ca0562a5d951e93b4e2b08f69625be141d
Николай <GultUy00@gmail.com>
2016-07-22 16:22:48 +0300

    Add exception classes for best overload resolver

---
+++ CodeContracts.Fody/BestOverloadResolvers/BestOverloadAmbiguousMethodsException.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.BestOverloadResolvers
+{
+    /// <summary>
+    /// Represents exception for ambiguous overload methods while resolves best oveload method
+    /// </summary>
+    [Serializable]
+    public class BestOverloadAmbiguousMethodsException : CodeContractsFodyException
+    {
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadAmbiguousMethodsException"/>
+        /// </summary>
+        public BestOverloadAmbiguousMethodsException()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadAmbiguousMethodsException"/>
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        public BestOverloadAmbiguousMethodsException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadAmbiguousMethodsException"/>
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        /// <param name="inner">The exception that is the cause of the current exception</param>
+        public BestOverloadAmbiguousMethodsException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadAmbiguousMethodsException"/>
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination</param>
+        protected BestOverloadAmbiguousMethodsException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadAmbiguousMethodsException"/>
+        /// </summary>
+        /// <param name="allMethodReferences">Collection of all overload methods</param>
+        /// <param name="ambiguousMethodReferences">Collection of ambiguous overload methods </param>
+        public BestOverloadAmbiguousMethodsException(IReadOnlyCollection<MethodReference> allMethodReferences, IReadOnlyCollection<MethodReference> ambiguousMethodReferences)
+            : base(FormatMessage(ambiguousMethodReferences))
+        {
+            AllMethodReferences = allMethodReferences;
+            AmbiguousMethodReferences = ambiguousMethodReferences;
+        }
+
+        /// <summary>
+        /// Collection of all overload methods
+        /// </summary>
+        public IReadOnlyCollection<MethodReference> AllMethodReferences { get; }
+
+        /// <summary>
+        /// Collection of ambiguous overload methods 
+        /// </summary>
+        public IReadOnlyCollection<MethodReference> AmbiguousMethodReferences { get; }
+        
+        /// <summary>
+        /// Formats error message for exception
+        /// </summary>
+        /// <param name="ambiguousMethodReferences">Collection of ambiguous overload methods</param>
+        /// <returns>Formatted error message</returns>
+        private static string FormatMessage(IReadOnlyCollection<MethodReference> ambiguousMethodReferences)
+        {
+            return 
+                "Ambiguous select best overload method from methods:" + Environment.NewLine +
+                string.Join(Environment.NewLine, ambiguousMethodReferences);
+        }
+    }
+}
---
+++ CodeContracts.Fody/BestOverloadResolvers/BestOverloadMissingMethodsException.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace CodeContracts.Fody.BestOverloadResolvers
+{
+    /// <summary>
+    /// Represents exception for missing overload methods while resolves best oveload method
+    /// </summary>
+    [Serializable]
+    public class BestOverloadMissingMethodsException : CodeContractsFodyException
+    {
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadMissingMethodsException"/>
+        /// </summary>
+        public BestOverloadMissingMethodsException()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadMissingMethodsException"/>
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        public BestOverloadMissingMethodsException(string message) 
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadMissingMethodsException"/>
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        /// <param name="inner">The exception that is the cause of the current exception</param>
+        public BestOverloadMissingMethodsException(string message, Exception inner) 
+            : base(message, inner)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadMissingMethodsException"/>
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination</param>
+        protected BestOverloadMissingMethodsException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of class <see cref="BestOverloadMissingMethodsException"/>
+        /// </summary>
+        /// <param name="allMethodReferences">Collection of all overload methods</param>
+        public BestOverloadMissingMethodsException(IReadOnlyCollection<MethodReference> allMethodReferences)
+            : base(FormatMessage(allMethodReferences))
+        {
+            AllMethodReferences = allMethodReferences;
+        }
+
+        /// <summary>
+        /// Collection of all overload methods
+        /// </summary>
+        public IReadOnlyCollection<MethodReference> AllMethodReferences { get; }
+
+        /// <summary>
+        /// Formats error message for exception
+        /// </summary>
+        /// <param name="allMethodReferences">Collection of all overload methods</param>
+        /// <returns>Formatted error message</returns>
+        private static string FormatMessage(IReadOnlyCollection<MethodReference> allMethodReferences)
+        {
+            return
+                "Can not find best overload method from methods:" + Environment.NewLine +
+                string.Join(Environment.NewLine, allMethodReferences);
+        }
+    }
+}
