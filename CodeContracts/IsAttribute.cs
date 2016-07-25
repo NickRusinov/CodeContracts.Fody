e5d9409faa069ac682922381670308141d5bb9ff
Николай <GultUy00@gmail.com>
2016-07-25 12:54:21 +0300

    Add is contract attribute

---
+++ CodeContracts/IsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace CodeContracts
+{
+    [Conditional("CONTRACTS_FULL")]
+    [AttributeUsage(DefaultUsages, AllowMultiple = true, Inherited = false)]
+    [ContractException(typeof(ArgumentException))]
+    public class IsAttribute : ContractAttribute
+    {
+        public IsAttribute(object type) { }
+
+        public IsAttribute(object arg, object type) { }
+
+        [Pure]
+        public static bool Validate(object arg, Type type) => type?.IsAssignableFrom(arg?.GetType()) ?? false;
+    }
+}
