bd660d1446fe311f1b56fe3e955c19606b79a6ec
Николай <gultuy00@gmail.com>
2016-08-12 12:02:43 +0300

    Add integration tests with custom contract invariant method

---
+++ CodeContracts.Fody.IntegrationTests/NotClean/ConcreteClassWithInvariantTests.cs
@@ -0,0 +1,39 @@
+﻿extern alias NotClean;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotClean::TestFoundations.IntegrationTests.Shared;
+using Xunit;
+
+namespace CodeContracts.Fody.IntegrationTests.NotClean
+{
+    public class ConcreteClassWithInvariantTests
+    {
+        [Fact]
+        public void FieldNotNullInvariantTest()
+        {
+            ContractAssert.With<ConcreteClassWithInvariant>()
+                .Success(sut => sut.FieldNotNullInvariant = new object())
+                .Success(sut => sut.InvokeInvariantMethod());
+
+            ContractAssert.With<ConcreteClassWithInvariant>()
+                .Success(sut => sut.FieldNotNullInvariant = null)
+                .Fail(sut => sut.InvokeInvariantMethod());
+        }
+
+        [Fact]
+        public void FieldNotEquals77Test()
+        {
+            ContractAssert.With<ConcreteClassWithInvariant>()
+                .Success(sut => sut.FieldNotEquals77 = 88)
+                .Success(sut => sut.InvokeInvariantMethod());
+
+            ContractAssert.With<ConcreteClassWithInvariant>()
+                .Success(sut => sut.FieldNotEquals77 = 77)
+                .Fail(sut => sut.InvokeInvariantMethod());
+        }
+    }
+}
---
+++ TestFoundations/TestFoundations.IntegrationTests.Shared/ConcreteClassWithInvariant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+using CodeContracts;
+
+namespace TestFoundations.IntegrationTests.Shared
+{
+    public class ConcreteClassWithInvariant
+    {
+        public object FieldNotNullInvariant = new object();
+
+        [NotEquals(77)]
+        public int FieldNotEquals77;
+
+        public void InvokeInvariantMethod()
+        {
+            
+        }
+
+        [ContractInvariantMethod]
+        private void InvariantMethod()
+        {
+            Contract.Invariant(FieldNotNullInvariant != null);
+        }
+    }
+}
