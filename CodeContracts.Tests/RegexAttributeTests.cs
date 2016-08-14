e77d145190f3f5ef0c555a115e8c8dd1d505707f
Николай <gultuy00@gmail.com>
2016-08-14 21:34:59 +0300

    Add regex contract attribute

---
+++ CodeContracts.Tests/RegexAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeContracts.Tests
+{
+    public class RegexAttributeTests
+    {
+        [Theory(DisplayName = "Проверка атрибута контракта Negative")]
+        [InlineData("aaa", "^\\d{3}$", false)]
+        [InlineData("111", "^\\d{3}$", true)]
+        public void ValidateArgRegexTest(string arg, string regex, bool isValidExpected)
+        {
+            var isValid = RegexAttribute.Validate(arg, regex);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+    }
+}
---
+++ CodeContracts/RegexAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeContracts
+{
+    [Conditional("CONTRACTS_FULL")]
+    [AttributeUsage(DefaultUsages, AllowMultiple = true, Inherited = false)]
+    [ContractException(typeof(ArgumentException))]
+    public sealed class RegexAttribute : ContractAttribute
+    {
+        public RegexAttribute(object regex) { }
+
+        public RegexAttribute(object arg, object regex) { }
+
+        [Pure]
+        public static bool Validate(string arg, string regex) => Regex.IsMatch(arg, regex);
+    }
+}
