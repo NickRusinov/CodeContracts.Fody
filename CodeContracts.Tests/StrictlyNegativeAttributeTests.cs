0612c24f39ceb967e4bd5915206f10c6a2a7c214
Николай <gultuy00@gmail.com>
2016-08-25 21:40:28 +0300

    Add strictly positive and negative contract attributes

---
+++ CodeContracts.Tests/StrictlyNegativeAttributeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeContracts.Tests
+{
+    public class StrictlyNegativeAttributeTests
+    {
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyNegative")]
+        [InlineData(1, false)]
+        [InlineData(0, false)]
+        [InlineData(-1, true)]
+        public void ValidateArgIntTest(int arg, bool isValidExpected)
+        {
+            var isValid = StrictlyNegativeAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyNegative")]
+        [InlineData(1u, false)]
+        [InlineData(0u, false)]
+        public void ValidateArgUIntTest(uint arg, bool isValidExpected)
+        {
+            var isValid = StrictlyNegativeAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyNegative")]
+        [InlineData(1L, false)]
+        [InlineData(0L, false)]
+        [InlineData(-1L, true)]
+        public void ValidateArgLongTest(long arg, bool isValidExpected)
+        {
+            var isValid = StrictlyNegativeAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyNegative")]
+        [InlineData(1uL, false)]
+        [InlineData(0uL, false)]
+        public void ValidateArgULongTest(ulong arg, bool isValidExpected)
+        {
+            var isValid = StrictlyNegativeAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+    }
+}
---
+++ CodeContracts.Tests/StrictlyPositiveAttributeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeContracts.Tests
+{
+    public class StrictlyPositiveAttributeTests
+    {
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyPositive")]
+        [InlineData(-1, false)]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        public void ValidateArgIntTest(int arg, bool isValidExpected)
+        {
+            var isValid = StrictlyPositiveAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyPositive")]
+        [InlineData(0u, false)]
+        [InlineData(1u, true)]
+        public void ValidateArgUIntTest(uint arg, bool isValidExpected)
+        {
+            var isValid = StrictlyPositiveAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyPositive")]
+        [InlineData(-1L, false)]
+        [InlineData(0L, false)]
+        [InlineData(1L, true)]
+        public void ValidateArgLongTest(long arg, bool isValidExpected)
+        {
+            var isValid = StrictlyPositiveAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Theory(DisplayName = "Проверка атрибута контракта StrictlyPositive")]
+        [InlineData(0uL, false)]
+        [InlineData(1uL, true)]
+        public void ValidateArgULongTest(ulong arg, bool isValidExpected)
+        {
+            var isValid = StrictlyPositiveAttribute.Validate(arg);
+
+            Assert.Equal(isValidExpected, isValid);
+        }
+    }
+}
